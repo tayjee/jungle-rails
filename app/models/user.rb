61aed700d5e512bef8ec375f8bd6f4f343860ad9
Jimmy Tang <jimmy.tang.360@gmail.com>
2022-03-16 15:14:12 +0000

    Add validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ActiveRecord::Base
 
   has_secure_password
-  validates :email, :uniqueness => true
-  
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, :uniqueness {case_sensitive: false}=> true, presence: true
+  validates :password, presence: true, confirmation: true
+  validates :password_confirmation, presence: true 
+
+
 end
