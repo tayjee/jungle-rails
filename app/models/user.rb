5968f861f69f89919d7872f8515852d2c9df09cb
Jimmy Tang <jimmy.tang.360@gmail.com>
2022-03-16 15:39:23 +0000

    Add multiple user validation tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,11 @@ class User < ActiveRecord::Base
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, uniqueness: {case_sensitive: false}, presence: true
-  validates :password, presence: true, confirmation: true
+  validates :password, presence: true, confirmation: true, length: {minimum: 6}
   validates :password_confirmation, presence: true 
 
+  def authenticate_with_credentials(email, password)
+    User.find_by_email(email.strip)&.authenticate(password) || nil
+  end
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   describe 'Validation' do
     before do
-      @user1 = User.new(first_name: 'Jimmy', last_name: 'Tang', email: 'JimmyTang@gmail.com', password: 'password123', password_confirmation: 'password123')
+      @user1 = User.new(first_name: "Jimmy", last_name: "Tang", email: "JimmyTang@gmail.com", password: "password123", password_confirmation: "password123")
     end
 
     it 'should be saved successfully with all required fields set' do
@@ -17,10 +17,40 @@ RSpec.describe User, type: :model do
       expect(@user2.errors.full_messages).to include("Password can't be blank")
     end
 
-    it 'should return an error if password is empty' do
+    it 'should return an error if password does not match confirmation' do
       @user3 = User.new(first_name: "Limmy", last_name: "Lang", email:"LimmyLang@gmail.com", password: "password321", password_confirmation: "password123")
       @user3.save
       expect(@user3.errors.full_messages).to include("Password confirmation doesn't match Password")
+    end
+
+    it 'should return an error if password does not meet minimum length' do
+      @user4 = User.new(first_name: "Zimmy", last_name: "Zang", email:"ZimmyZang@gmail.com", password: "12345", password_confirmation: "password123")
+      @user4.save
+      expect(@user4.errors.full_messages).to include("Password is too short (minimum is 6 characters)")
+    end
+
+    describe '.authenticate_with_credentials' do
+      # examples for this class method here
+      it 'should be successful if credentials are correct' do
+        expect(@user1.authenticate_with_credentials("JimmyTang@gmail.com", "password123"))
+      end
+
+      it 'should be successful if credentials are correct but email contains extra white space' do
+        expect(@user1.authenticate_with_credentials("   JimmyTang@gmail.com   ", "password123"))
+      end
+
+      it 'should be successful if credentials are correct but email contains different capitalization' do
+        expect(@user1.authenticate_with_credentials("JiMmYTaNg@gmail.com", "password123"))
+      end
+
+      it 'should be NIL if email is incorrect' do
+        expect(@user1.authenticate_with_credentials("WrongEmail@gmail.com", "password123"))
+      end
+
+      it 'should be NIL if password is incorrect' do
+        expect(@user1.authenticate_with_credentials("JimmyTang@gmail.com", "incorrectpassword123"))
+      end
+
     end
 
   end
