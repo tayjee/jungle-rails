aba826daa9e5ab483627e50287be3d606235093a
Jimmy Tang <jimmy.tang.360@gmail.com>
2022-03-16 15:22:18 +0000

    Add test for empty password and non-matching passwords

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,5 +10,18 @@ RSpec.describe User, type: :model do
       @user1.save
       expect(@user1.errors.full_messages).to be_empty
     end
+
+    it 'should return an error if password is empty' do
+      @user2 = User.new(first_name: "Timmy", last_name: "Jang", email:"TimmyJang@gmail.com", password_confirmation: "password123")
+      @user2.save
+      expect(@user2.errors.full_messages).to include("Password can't be blank")
+    end
+
+    it 'should return an error if password is empty' do
+      @user3 = User.new(first_name: "Limmy", last_name: "Lang", email:"LimmyLang@gmail.com", password: "password321", password_confirmation: "password123")
+      @user3.save
+      expect(@user3.errors.full_messages).to include("Password confirmation doesn't match Password")
+    end
+
   end
 end
