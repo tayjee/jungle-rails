92788dec3f58220f7214b7a8eae19bb4134eb6af
Jimmy Tang <jimmy.tang.360@gmail.com>
2022-03-16 15:17:01 +0000

    Fix some validations, add basic save test

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   has_secure_password
   validates :first_name, presence: true
   validates :last_name, presence: true
-  validates :email, :uniqueness {case_sensitive: false}=> true, presence: true
+  validates :email, uniqueness: {case_sensitive: false}, presence: true
   validates :password, presence: true, confirmation: true
   validates :password_confirmation, presence: true 
 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Validation' do
+    before do
+      @user1 = User.new(first_name: 'Jimmy', last_name: 'Tang', email: 'JimmyTang@gmail.com', password: 'password123', password_confirmation: 'password123')
+    end
+
+    it 'should be saved successfully with all required fields set' do
+      @user1.save
+      expect(@user1.errors.full_messages).to be_empty
+    end
+  end
 end
